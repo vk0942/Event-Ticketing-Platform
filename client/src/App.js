6c6621752f385e9cf71a721223e0a795f2351df2
vk0942 <vanshkathnawal0942@gmail.com>
2023-09-05 11:55:43 +0530

    vansh's initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import 'flowbite';
+import  Navigation from './components/Navigation';
 
 function App() {
   return (
 
     <BrowserRouter>
+        <Navigation/>
         <Routes>
             <Route 
                 path='/'
